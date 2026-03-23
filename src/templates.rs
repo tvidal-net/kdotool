a99e5ef30ade4fe94d9d399b423440b9196479fb
thiago <thiago@tvidal.uk>
2026-03-23 18:45:15 +0000

    remove empty debug message
    (cherry picked from commit c163ecf38d633b541baab5171fddbf0b9a887f4b)

--- src/templates.rs
+++ src/templates.rs
@@ -13,7 +13,9 @@ function output_debug(message) {
 }
 
 function output_error(message) {
-    print("{{{marker}}} ERROR", message);
+    if (message) {
+        print("{{{marker}}} ERROR", message);
+    }
     callDBus("{{{dbus_addr}}}", "/", "", "error", message.toString());
 }
 
